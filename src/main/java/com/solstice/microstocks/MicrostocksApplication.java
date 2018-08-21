3880a7053abb3b813e0e67dc335dcfc4a7ac1e89
Matt Nicolay <mnicolay@solstice.com>
2018-08-21 13:56:50 -0500

    initial commit

---
+++ src/main/java/com/solstice/microstocks/MicrostocksApplication.java
@@ -0,0 +1,12 @@
+package com.solstice.microstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrostocksApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicrostocksApplication.class, args);
+  }
+}
