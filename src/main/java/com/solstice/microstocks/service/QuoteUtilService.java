e60a556e2b01ecd8a1facd5b34e10b69482a770e
Matt Nicolay <mnicolay@solstice.com>
2018-08-28 09:08:23 -0500

    move date methods into DateService and refactor accordingly

--- src/main/java/com/solstice/microstocks/service/QuoteUtilService.java
+++ src/main/java/com/solstice/microstocks/service/QuoteUtilService.java
@@ -16,17 +16,19 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class QuoteUtilService {
 
-  private final long ONE_DAY_IN_MILLISECONDS = 86400000L;
 
   private QuoteRepository quoteRepository;
   private RestTemplate restTemplate;
   private EurekaClient eurekaClient;
+  private DateService dateService;
 
   public QuoteUtilService(QuoteRepository quoteRepository,
-      RestTemplate restTemplate, EurekaClient eurekaClient) {
+      RestTemplate restTemplate, EurekaClient eurekaClient,
+      DateService dateService) {
     this.quoteRepository = quoteRepository;
     this.restTemplate = restTemplate;
     this.eurekaClient = eurekaClient;
+    this.dateService = dateService;
   }
 
   public AggregateQuote getAggregate(
@@ -36,8 +38,8 @@ public class QuoteUtilService {
       String pattern) throws NullPointerException, ParseException {
 
     int symbolId = getIdFromSymbolService(symbol);
-    Date fromDate = parseDate(dateString, pattern);
-    Date toDate = getNext(timePeriod, fromDate);
+    Date fromDate = dateService.parseDate(dateString, pattern);
+    Date toDate = dateService.getNext(timePeriod, fromDate);
 
     return quoteRepository.getAggregateData(symbolId, fromDate, toDate);
   }
@@ -52,29 +54,5 @@ public class QuoteUtilService {
         .getHomePageUrl();
   }
 
-  private Date parseDate(String dateString, String pattern) throws ParseException {
-    return new SimpleDateFormat(pattern).parse(dateString);
-  }
 
-  private Date getNext(TimePeriod timePeriod, Date date) {
-    switch(timePeriod) {
-      case DAY:
-        return getNextDay(date);
-      case MONTH:
-        return getNextMonth(date);
-      default:
-        return null;
-    }
-  }
-
-  private Date getNextDay(Date date) {
-    return new Date(date.getTime() + ONE_DAY_IN_MILLISECONDS);
-  }
-
-  private Date getNextMonth(Date date) {
-    Calendar calendar = new GregorianCalendar();
-    calendar.setTime(date);
-    long monthOffset = ONE_DAY_IN_MILLISECONDS * calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-    return new Date(date.getTime() + monthOffset);
-  }
 }
