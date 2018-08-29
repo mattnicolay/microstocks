e9cd01376248bae2a674bddba5ff51b1ff7828e4
Matt Nicolay <mnicolay@solstice.com>
2018-08-29 14:01:47 -0500

    implement integration tests for Controllers

--- src/test/java/com/solstice/microstocks/controller/LoadControllerIntegrationTest.java
+++ src/test/java/com/solstice/microstocks/controller/LoadControllerIntegrationTest.java
@@ -5,98 +5,38 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.solstice.microstocks.model.Quote;
 import com.solstice.microstocks.repository.QuoteRepository;
-import com.solstice.microstocks.service.LoadUtilService;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.util.ReflectionTestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
-    DbUnitTestExecutionListener.class})
-@DatabaseSetup("classpath:test-dataset.xml")
 public class LoadControllerIntegrationTest {
 
   @Autowired
   private QuoteRepository quoteRepository;
 
   @Autowired
-  private LoadUtilService loadUtilService;
-
   private LoadController loadController;
-  private SimpleDateFormat dateFormat;
-
-  @Before
-  public void setup() {
-    try {
-      ReflectionTestUtils.setField(loadUtilService, "datasetUrl",
-          new URL("https://bootcamp-training-files.cfapps.io/week4/week4_stocks.json"));
-    } catch (MalformedURLException e) {
-      e.printStackTrace();
-    }
-    loadController = new LoadController(loadUtilService);
-    dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-  }
 
   @Test
   public void testPostLoad() {
-//    try {
-//      ResponseEntity<List<Quote>> quoteResponse = loadController.load();
-//
-//
-//      List<Quote> quotes = quoteResponse.getBody();
-//      when(quoteRepository.findAll()).thenReturn(quotes);
-//      List<Quote> quotesFromDb = quoteRepository.findAll();
-//
-//      assertThat(quoteResponse.getStatusCodeValue(), is(equalTo(201)));
-//      assertThat(quoteResponse.getBody(), is(notNullValue()));
-//      assertFalse(quoteResponse.getBody().isEmpty());
-//      assertThat(quotes, is(notNullValue()));
-//      assertFalse(quotes.isEmpty());
-//      quoteResponse.getBody().forEach(quote -> assertTrue(quotes.contains(quote)));
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
-  }
+    ResponseEntity<List<Quote>> quoteResponse = loadController.load();
 
-  @Test
-  public void testGetLoad() throws ParseException {
 
-//    List<Quote> expectedQuotes = Arrays.asList(
-//        new Quote(2, 1111.11, 111, dateFormat.parse("2018-06-22 08:30:00")),
-//        new Quote(2, 2222.22, 111, dateFormat.parse("2018-06-22 10:00:00")),
-//        new Quote(2, 3333.33, 111, dateFormat.parse("2018-06-22 12:00:00")),
-//        new Quote(2, 4444.44, 111, dateFormat.parse("2018-06-22 13:00:00")),
-//        new Quote(2, 5555.55, 111, dateFormat.parse("2018-06-22 14:00:00")),
-//        new Quote(2, 6666.66, 111, dateFormat.parse("2018-06-22 16:30:00"))
-//    );
-//    List<Quote> quotes = loadController.getStocks().getBody();
-//
-//    assertThat(quotes, is(notNullValue()));
-//    assertFalse(quotes.isEmpty());
-//    quotes.forEach(quote -> assertTrue(expectedQuotes.contains(quote)));
+    List<Quote> quotes = quoteResponse.getBody();
+    List<Quote> quotesFromDb = quoteRepository.findAll();
+
+    assertThat(quoteResponse.getStatusCodeValue(), is(equalTo(201)));
+    assertThat(quotes, is(notNullValue()));
+    assertFalse(quotes.isEmpty());
+    quotes.forEach(quote -> assertTrue(quotesFromDb.contains(quote)));
   }
 }
--- src/test/java/com/solstice/microstocks/controller/QuoteControllerIntegrationTest.java
+++ src/test/java/com/solstice/microstocks/controller/QuoteControllerIntegrationTest.java
@@ -1,142 +1,112 @@
 package com.solstice.microstocks.controller;
 
-//import static org.hamcrest.CoreMatchers.any;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.Mockito.any;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.*;
 
 import com.solstice.microstocks.model.AggregateQuote;
+import com.solstice.microstocks.model.Quote;
 import com.solstice.microstocks.repository.QuoteRepository;
-import com.solstice.microstocks.service.QuoteUtilService;
-import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(MockitoJUnitRunner.class)
-@WebMvcTest(QuoteController.class)
-//@DatabaseSetup("classpath:")
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class QuoteControllerIntegrationTest {
 
-  @Mock
+  @Autowired
   private QuoteRepository quoteRepository;
 
-  @Mock
-  private QuoteUtilService quoteUtilService;
-
-  private MockMvc mockMvc;
-
-  @InjectMocks
+  @Autowired
   private QuoteController quoteController;
+
+  @Autowired
+  private LoadController loadController;
 
   @Before
   public void setup() {
-    MockitoAnnotations.initMocks(this);
-    mockMvc = MockMvcBuilders.standaloneSetup(quoteController).build();
-  }
-
-  @Test
-  public void testGetAggregateDaily() throws Exception {
-//    try {
-//      Date testDateStart = new Date(1530019800);
-//      Date testDateEnd = new Date(1529703000);
-//      AggregateQuote expected = new AggregateQuote(
-//          "GOOG",
-//          4000,
-//          1000,
-//          4000,
-//          1000,
-//          testDateEnd);
-//
-//      Symbol mockSymbol = new Symbol("GOOG");
-//
-//      entityManager.persist(mockSymbol);
-//
-//      List<Quote> mockQuotes = Arrays.asList(
-//          new Quote(mockSymbol, 1000, 100, testDateStart),
-//          new Quote(mockSymbol, 2000, 200, testDateStart),
-//          new Quote(mockSymbol, 3000, 300, testDateEnd),
-//          new Quote(mockSymbol, 4000, 400, testDateEnd)
-//      );
-//
-//      mockQuotes.forEach(quote -> entityManager.persist(quote));
-//
-//      when(quoteRepository.getAggregateData(anyInt(), any(Date.class), any(Date.class))).thenReturn(expected);
-//
-//
-//
-//      assertThat(quote, is(equalTo(expected)));
-//
-//      assertThat(quote.getSymbolId(), is(equalTo("GOOG")));
-//      assertThat(quote.getMaxPrice(), is(equalTo(1130.99)));
-//      assertThat(quote.getMinPrice(), is(equalTo(1120.01)));
-//      assertThat(quote.getClosingPrice(), is(equalTo(1122.57)));
-//      assertThat(quote.getTotalVolume(), is(equalTo(724223)));
-//      assertThat(quote.getDate().toString(), is(equalTo("2018-06-22T16:30:00.000+0000")));
-//    } catch (ParseException e) {
-//      e.printStackTrace();
-//    }
-
-    mockMvc.perform(get("/daily/GOOG/2016-06-22")).andExpect(status().isOk()).andReturn();
-  }
-
-  @Test
-  public void testGetAggregateDailyEmptyDataset() {
-    try {
-      AggregateQuote quote = quoteController.getAggregateDaily("GOOG", "2018-06-22");
-
-      assertThat(quote, is(nullValue()));
-    } catch (Exception e) {
-      e.printStackTrace();
+    if (quoteRepository.findAll().isEmpty()) {
+      loadController.load();
     }
   }
 
+  /*
+    Requires that symbol service is up and running
+  */
   @Test
-  public void testGetAggregateMonthly() throws Exception {
-//    try {
-//      AggregateQuote expected = new AggregateQuote(
-//          "GOOG",
-//          1130.99,
-//          1120,
-//          1129.65,
-//          2159363,
-//          new Date(1530048600));
-//
-//      AggregateQuote quote = quoteController.getAggregateMonthly("GOOG", "2018-06");
-//
-//      assertThat(quote, is(equalTo(expected)));
-//
-//      assertThat(quote.getSymbolId(), is(equalTo("GOOG")));
-//      assertThat(quote.getMaxPrice(), is(equalTo(1130.99)));
-//      assertThat(quote.getMinPrice(), is(equalTo(1120)));
-//      assertThat(quote.getClosingPrice(), is(equalTo(1129.65)));
-//      assertThat(quote.getTotalVolume(), is(equalTo(2159363)));
-//      assertThat(quote.getDate().toString(), is(equalTo("2018-06-26T16:30:00.000+0000")));
-//    } catch (ParseException e) {
-//      e.printStackTrace();
-//    }
-    mockMvc.perform(get("/monthly/GOOG/2016-06")).andExpect(status().isOk()).andReturn();
+  public void testGetAggregateDaily() {
+    AggregateQuote expected = new AggregateQuote(
+        "GOOG",
+        1130.98,
+        1120,
+        1125.07,
+        763726,
+        LocalDate.of(2018, 6, 22));
+
+    AggregateQuote quote = quoteController.getAggregateDaily("GOOG", "2018-06-22");
+
+    assertThat(quote, is(notNullValue()));
+
+    assertThat(quote.getName(), is(equalTo(expected.getName())));
+    assertThat(quote.getMaxPrice(), is(equalTo(expected.getMaxPrice())));
+    assertThat(quote.getMinPrice(), is(equalTo(expected.getMinPrice())));
+    assertThat(quote.getClosingPrice(), is(equalTo(expected.getClosingPrice())));
+    assertThat(quote.getTotalVolume(), is(equalTo(expected.getTotalVolume())));
+    assertThat(quote.getDate(), is(equalTo(expected.getDate())));
+  }
+
+  /*
+  Requires that symbol service is up and running
+   */
+  @Test
+  public void testGetAggregateMonthly() {
+    AggregateQuote expected = new AggregateQuote(
+        "GOOG",
+        1130.99,
+        1120,
+        1125.52,
+        2302903,
+        LocalDate.of(2018, 6, 26));
+
+    AggregateQuote quote = quoteController.getAggregateMonthly("GOOG", "2018-06");
+
+    assertThat(quote, is(notNullValue()));
+
+    assertThat(quote.getName(), is(equalTo(expected.getName())));
+    assertThat(quote.getMaxPrice(), is(equalTo(expected.getMaxPrice())));
+    assertThat(quote.getMinPrice(), is(equalTo(expected.getMinPrice())));
+    assertThat(quote.getClosingPrice(), is(equalTo(expected.getClosingPrice())));
+    assertThat(quote.getTotalVolume(), is(equalTo(expected.getTotalVolume())));
+    assertThat(quote.getDate(), is(equalTo(expected.getDate())));
   }
 
   @Test
-  public void testGetAggregateMonthlyEmptyDataset() {
-    try {
-      AggregateQuote quote = quoteController.getAggregateMonthly("GOOG", "2018-06");
+  public void testGetQuotes() {
+    ResponseEntity<List<Quote>> quoteResponse = quoteController.getQuotes();
+    List<Quote> quotesFromDb = quoteRepository.findAll();
 
-      assertThat(quote, is(nullValue()));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    assertThat(quoteResponse, is(notNullValue()));
+    assertThat(quoteResponse.getBody(), is(notNullValue()));
+
+    List<Quote> quotes = quoteResponse.getBody();
+
+    assertFalse(quotes.isEmpty());
+    quotes.forEach(quote -> assertTrue(quotesFromDb.contains(quote)));
+  }
+
+  @Test
+  public void testDeleteQuotes() {
+    quoteController.deleteQuotes();
+
+    assertTrue(quoteRepository.findAll().isEmpty());
   }
 }
