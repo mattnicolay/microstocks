adae1873534a31df715e8a4d546590766ad4b787
Matt Nicolay <mnicolay@solstice.com>
2018-08-29 13:49:55 -0500

    remove some unnecessary code in LoadController

--- src/main/java/com/solstice/microstocks/controller/LoadController.java
+++ src/main/java/com/solstice/microstocks/controller/LoadController.java
@@ -4,11 +4,8 @@ import com.solstice.microstocks.model.Quote;
 import com.solstice.microstocks.service.LoadUtilService;
 import java.io.IOException;
 import java.util.List;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +28,6 @@ public class LoadController {
       quotes = loadUtilService.loadQuotes();
     } catch (IOException e) {
       status = HttpStatus.INTERNAL_SERVER_ERROR;
-      e.printStackTrace();
     }
     return new ResponseEntity<>(quotes, status);
   }
